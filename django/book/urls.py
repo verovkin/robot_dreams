2266256e32f6206eddb2a68fed1cd8714b6f2e47
Dmytro Verovkin <dmitry335@gmail.com>
2023-04-15 14:44:51 +0300

    done

--- django/book/urls.py
+++ django/book/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.show_users, name='index')
+    path('', views.show_books, name='index')
 ]
--- django/robot/urls.py
+++ django/robot/urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('user.urls')),
+    path('books/', include('book.urls')),
+    path('purchases/', include('purchase.urls')),
 ]

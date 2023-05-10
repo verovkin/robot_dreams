a9d5a9ffb79b0245b58a791027f2861559ee94b1
Dmytro Verovkin <dmitry335@gmail.com>
2023-05-10 16:57:39 +0300

    tasks: - for user creation - for retrieve a user by id - scheduled

--- django/user/views.py
+++ django/user/views.py
@@ -3,10 +3,12 @@ from rest_framework import filters
 from django.views.generic import ListView, CreateView, DetailView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from user.forms import UserForm
 from user.models import User
 from user.serializers import UserSerializer
+from user.tasks import my_task_list, my_task_retrieve, my_task_user_create
 
 
 class UserListView(ListView):
@@ -20,6 +22,7 @@ class UserDetailView(DetailView):
 class UserCreateView(CreateView):
     model = User
     form_class = UserForm
+
 
 # Filter
 class UserFilter(django_filters.FilterSet):
@@ -50,3 +53,29 @@ class UserViewSet(ModelViewSet):
         django_filters.rest_framework.DjangoFilterBackend,
         filters.SearchFilter,
     ]
+
+    # This one works!
+    def perform_create(self, serializer):
+        my_task_user_create.delay()
+        serializer.save()
+
+    # why this one does not work?
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        my_task_list.delay()  # calling a task
+        print("yo")
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        my_task_retrieve.delay(instance.id)
+        print("popo")
+        return Response(serializer.data)
